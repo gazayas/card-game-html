cac28dbfa7bcb1c2525f381348f1801b909a7708
gazayas <g-zayas@hotmail.com>
2018-05-23 15:46:28 +0900

    edit files

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@
 class Main {
 	constructor() {
 		this.deck = new Deck()
-		this.person = new Person()
-		this.computer = new Person()
+		//this.person = new Person()
+		//this.computer = new Person()
 	}
 
 	start() {
@@ -13,11 +13,52 @@ class Main {
 		// Select stage of progress (current season)
 		// pass out stuff
 	}
-
+/*
 	shuffleDeck() {
-
+		// Generate array of indices with
+		var indices = this.CreateIndicesWithLengthOf(40)
+		// Make new array and clear deck.cards
+		// Push randomized ones to 
 	}
 
+	CreateIndicesWithLengthOf(number) {
+		// Populate binary tree
+		//var tree = [[][20],[7, 15], [25, 45], [3, 10], [20, 30], [40, 50]]
+		var tree = []
+		for(var i = 0; i < number; i++) {
+			var no_new_index = true
+
+			while(no_new_index) {
+				var node = 1;
+				var index = Math.floor(Math.random() * 40)
+				var search_not_finished = true;
+
+				while(search_not_finished) {
+					if (tree[node] == undefined) {
+						tree[node] = [number]
+						no_new_index = false
+						search_not_finished = false
+					} else if(tree[node].length == 1 || tree[node] == null) {
+						tree[node] = [number]
+						no_new_index = false
+						search_not_finished = false
+					} else if(number == tree[node][0] || number == tree[node][1]) {
+						search_not_finished = false
+						// Restart index generation
+					} else if(number < tree[node][0]) {
+						node = node * 2
+					} else if(number > tree[node][1]) {
+						node = node * 2 + 1
+					} 
+				}
+			}
+		}
+
+		console.log(tree)
+		return tree
+	}
+
+*/
 	dealCards() {
 
 	}
---
+++ scripts/person.js
@@ -0,0 +1,14 @@
+class Person {
+	constructor(isAi = false) {
+		this.isAi = isAi
+		this.score = 0
+	}
+
+	Move() {
+		if(this.isAi){
+			// Automate
+		} else {
+			// wait for user input
+		}
+	}
+}
